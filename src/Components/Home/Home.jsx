65f9c5d30db87224e5ec12b3eeaef4e11812779c
Binard2112 <mait58674@gmail.com>
2025-04-11 10:06:50 +0700

    homePage done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,15 +5,20 @@ import Slider from 'react-slick'
 import { data } from 'react-router-dom'
 import vf8 from '../../assets/images/vf8.png'
 import vf9 from '../../assets/images/vf91.png'
+import footerbg from '../../assets/images/footerbg.png'
 
 export default function Home() {
   const apiEvent = 'https://67e227a797fc65f53534c8a2.mockapi.io/apiTodo/events'
   const apiLichSuThuongHieu = 'https://67f77fac42d6c71cca657140.mockapi.io/LichSuThuongHieu'
   const apiAward = 'https://67f77fac42d6c71cca657140.mockapi.io/Award'
+  const apiNew = 'https://67f85e102466325443ec7f89.mockapi.io/news'
+  const apiCar = 'https://67e227a797fc65f53534c8a2.mockapi.io/apiTodo/cars'
 
   const [dataEvent, setDataEvent] = useState([])
   const [dataLichSu, setDataLichSu] = useState([])
   const [dataAward, setDataAward] = useState([])
+  const [dataNew, setDataNew] = useState([])
+  const [dataCar, setDataCar] = useState([])
 
   useEffect(() => {
     fetch(apiEvent)
@@ -37,6 +42,24 @@ export default function Home() {
         return res.json()
       })
       .then((data) => setDataAward(data))
+    console.log(data)
+  }, [])
+
+  useEffect(() => {
+    fetch(apiNew)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => setDataNew(data))
+    console.log(data)
+  }, [])
+
+  useEffect(() => {
+    fetch(apiCar)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => setDataCar(data))
     console.log(data)
   }, [])
 
@@ -147,6 +170,77 @@ export default function Home() {
               )
             })}
           </Slider>
+        </div>
+      </div>
+      {/* ----------- */}
+      <div className='flex flex-col justify-center items-center mt-20 pb-10 bg-[#f2f2f2]'>
+        <h2 className='font-medium text-4xl mb-20 mt-20'>Các tin tức mới nhất tại chi nhánh Hồ Chí Minh</h2>
+        <div className='flex flex-wrap gap-10 pl-40 pr-40'>
+          {dataNew.map((item) => {
+            return (
+              <div
+                className='flex flex-col flex-1 box-border border-gray-300 shadow-lg rounded-xl p-4 basis-[calc(50%-20px)] border bg-[#fff]'
+                key={item.id}
+              >
+                <img src={item.avatar} alt='' />
+                <h2 className='p-3 font-bold hover:text-[#2d63ed] text-xl cursor-pointer'>{item.title}</h2>
+                <span className='p-3'>{item.content}</span>
+                <div className='flex justify-between mt-10 p-3'>
+                  <div className='flex gap-5'>
+                    <img src={item.imagePulisher} alt='' className='w-5 object-cover' />
+                    <span>{item.pulisher}</span>
+                  </div>
+                  <span>{item.releasedDate}</span>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      {/* ----------- */}
+      <div className='relative w-full overflow-hidden'>
+        <img src={footerbg} alt='' className='w-full h-full object-cover' />
+        <div className='absolute bottom-0 left-0 right-0 h-3/4 bg-gradient-to-t from-black/60 to-transparent z-0' />
+        <div className='absolute inset-0 flex justify-start p-10 mt-10 z-10'>
+          <div className='flex flex-col gap-5'>
+            <h2 className='font-medium text-white text-4xl'>SPEEDZONE HỒ CHÍ MINH</h2>
+            <div className='flex flex-col'>
+              <span className='text-white'>12 Nguyễn Văn Bảo, phường 1, quận Gò Vấp, TPHCM</span>
+              <span className='text-white'>____________________________________________________________</span>
+            </div>
+            <div className='flex flex-col'>
+              <span className='text-white'>Số điện thoại: 0837000222</span>
+              <span className='text-white'>____________________________________________________________</span>
+            </div>
+            <div className='flex flex-col'>
+              <span className='text-white'>Zalo: 0837000222</span>
+              <span className='text-white'>____________________________________________________________</span>
+            </div>
+            <div className='flex flex-col'>
+              <span className='text-white'>Gmail: Mait58674@gmail.com</span>
+              <span className='text-white'>____________________________________________________________</span>
+            </div>
+          </div>
+        </div>
+        <div className='absolute inset-0 flex justify-center p-10 mt-10 z-10'>
+          <div className='flex flex-col gap-5'>
+            <h2 className='font-medium text-white text-4xl'>SẢN PHẨM</h2>
+            <div>
+              {dataCar.map((item) => {
+                return (
+                  <div className='flex flex-col' key={item.id}>
+                    <h1 className='text-white '>{item.name}</h1>
+                    <span className='text-white'>____________________________________________________________</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+        <div className='absolute inset-0 flex justify-end p-10 mt-10 z-10'>
+          <div className='flex flex-col gap-5'>
+            <h2 className='font-medium text-white text-4xl'>SPEEDZONE HỒ CHÍ MINH</h2>
+          </div>
         </div>
       </div>
     </div>
